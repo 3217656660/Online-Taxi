9145de501d38c8a3fd6ffbbeafe40ca630e2297c
zhangxinyu <3217656660@qq.com>
2024-01-17 23:24:37 +0800

    1.0

--- consumer-feign-client4001/src/main/java/com/zxy/work/service/UserServiceClient.java
+++ consumer-feign-client4001/src/main/java/com/zxy/work/service/UserServiceClient.java
@@ -2,7 +2,7 @@ package com.zxy.work.service;
 
 
 
-import com.zxy.work.entities.CommonResult;
+
 import com.zxy.work.entities.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
