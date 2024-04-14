a739d2fdf0f9287d76c0d5d8c5d924fbbc6708e0
zhangxinyu <3217656660@qq.com>
2024-04-14 21:21:45 +0800

    项目重构2.1

--- provider-order-service9001/src/main/java/com/zxy/work/service/impl/OrderServiceImpl.java
+++ provider-order-service9001/src/main/java/com/zxy/work/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.zxy.work.dao.OrderMapper;
 import com.zxy.work.entities.MyException;
 import com.zxy.work.entities.Order;
 import com.zxy.work.service.OrderService;
+import com.zxy.work.util.cache.CacheUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,19 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private OrderMapper orderMapper;
 
+    @Resource
+    private CacheUtil redisUtil;
+
+    /**
+     * 设置通用缓存TTL(30分钟)
+     */
+    private static final int cacheTTL = 30 * 60;
+
+    /**
+     * 设置缓存通用key前缀
+     */
+    private static final String commonKey = "order:id:";
+
 
     /**
      * 创建订单
@@ -30,6 +44,7 @@ public class OrderServiceImpl implements OrderService {
         //1.查询数据库中是否还有待处理的订单
         //2.如果有待处理的订单，那么让用户去处理该订单
         //3.如果没有，则重新创建一个订单
+        String key;
         Order notSolve;
         try{
             notSolve = orderMapper.selectNotSolve(order.getUserId());
@@ -37,11 +52,22 @@ public class OrderServiceImpl implements OrderService {
             log.error("订单查询异常,msg={}", e.getMessage());
             throw new MyException("未处理的订单查询异常");
         }
-        if (notSolve != null)
+        if (notSolve != null){
+            key = commonKey + notSolve.getId();
+            redisUtil.set(key, notSolve, cacheTTL);
+            log.info("key={}未处理的订单加入缓存", key);
             throw new MyException("您还有未处理的订单");
+        }
 
         try{
-            return orderMapper.create(order);
+            int result = orderMapper.create(order.setStatus(0));
+            if (result == 1){
+                Order selectNotSolve = orderMapper.selectNotSolve(order.getUserId());
+                key = commonKey + selectNotSolve.getId();
+                redisUtil.set(key, selectNotSolve, cacheTTL);
+                log.info("key={}刚创建的订单加入缓存", key);
+            }
+            return result;
         }catch (Exception e){
             log.error("订单创建异常,msg={}", e.getMessage());
             throw new MyException("订单创建异常");
@@ -57,8 +83,14 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public int delete(Integer id) throws MyException{
+        String key = commonKey + id;
         try{
-            return orderMapper.delete(id);
+            int result = orderMapper.delete(id);
+            if (result == 1){
+                redisUtil.del(key);
+                log.info("key={},订单从缓存中移除", key);
+            }
+            return result;
         }catch (Exception e){
             log.error("订单取消异常,msg={}", e.getMessage());
             throw new MyException("订单取消异常");
@@ -74,8 +106,15 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public int update(Order order) throws MyException{
+        String key = commonKey + order.getId();
         try{
-            return orderMapper.update(order);
+            int result = orderMapper.update(order);
+            if (result == 1){
+                Order select = orderMapper.selectByOrderId(order.getId());
+                redisUtil.set(key, select, cacheTTL);
+                log.info("key={},更新信息加入缓存", key);
+            }
+            return result;
         }catch (Exception e){
             log.error("订单更新异常,msg={}", e.getMessage());
             throw new MyException("订单更新异常");
@@ -132,8 +171,20 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public Order selectByOrderId(Integer id) throws MyException{
+        String key = commonKey + id;
+        Object select = redisUtil.get(key);
+        if (select != null){
+            redisUtil.set(key ,select, cacheTTL);
+            return (Order) select;
+        }
+
         try{
-            return orderMapper.selectByOrderId(id);
+            Order order = orderMapper.selectByOrderId(id);
+            if (order != null) {
+                redisUtil.set(key, order, cacheTTL);
+                log.info("key={}订单加入缓存", key);
+            }
+            return order;
         }catch (Exception e){
             log.error("通过订单id的订单查询异常,msg={}", e.getMessage());
             throw new MyException("通过订单id的订单查询异常");
@@ -149,7 +200,13 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order selectNotSolve(Integer userId) throws MyException{
         try{
-            return orderMapper.selectNotSolve(userId);
+            Order order = orderMapper.selectNotSolve(userId);
+            if (order != null) {
+                String key = commonKey + order.getId();
+                redisUtil.set(key, order, cacheTTL);
+                log.info("key={}的未解决订单加入缓存", key);
+            }
+            return order;
         }catch (Exception e){
             log.error("通过订单id的订单查询异常,msg={}", e.getMessage());
             throw new MyException("通过订单id的订单查询异常");
